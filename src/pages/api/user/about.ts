06fbac3c4f40b1e63f404161ca982c9db11aa3de
Julie Souchet <jsouchet@easyvista.com>
2021-12-10 23:49:05 +0100

    [auth] Rename route to get current user

--- src/pages/api/user/about.ts
+++ src/pages/api/user/about.ts
@@ -1,7 +1,8 @@
 import { withSession } from '@middlewares/session.middleware';
+import { IUserPublic } from '@typing/user.interface';
 
 export default withSession(async (req, res) => {
   return res.json({
-    user: req.session.get('user')
+    user: req.session.get<IUserPublic>('user')
   })
 })
