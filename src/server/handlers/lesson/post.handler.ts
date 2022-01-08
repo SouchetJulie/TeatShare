4a675d55f3eb5b01ccc4cfee10bdae7c6b9f47cd
Julie Souchet <jsouchet@easyvista.com>
2022-01-08 11:34:44 +0100

    [internal] Format and comment

--- src/server/handlers/lesson/post.handler.ts
+++ src/server/handlers/lesson/post.handler.ts
@@ -1,4 +1,5 @@
 import { NextApiResponse } from 'next';
+
 import { withSession } from '@middlewares/session.middleware';
 import { createNewLesson } from '@services/lessons.service';
 import { ISessionApiRequest } from '@typing/session-api-request.interface';
@@ -8,21 +9,15 @@ import { IUserPublic } from '@typing/user.interface';
 const handler = async (req: ISessionApiRequest, res: NextApiResponse) => {
   const lesson: ILessonFile = req.body;
   if (!lesson.title) {
-    res.status(400).json({error: 'Veuillez remplir le formulaire.'});
+    res.status(400).json({ error: 'Veuillez remplir le formulaire.' });
   }
 
   const currentUser = req.session.get<IUserPublic>('user');
   const result = await createNewLesson(currentUser, lesson);
   if (result['id']) {
-    res.status(200).json({
-      success: true,
-      id: result['id']
-    });
+    res.status(200).json({ success: true, id: result['id'] });
   } else {
-    res.status(400).json({
-      success: false,
-      error: result['error'] || 'Lesson upload failed'
-    });
+    res.status(400).json({ success: false, error: result['error'] || 'Lesson upload failed' });
   }
 };
 
--- src/server/services/lessons.service.ts
+++ src/server/services/lessons.service.ts
@@ -1,55 +1,33 @@
-import { ObjectId } from "bson";
-import { getDatabase } from "./database.service";
-import { ILessonFile } from "@typing/lesson-file.interface";
-import { IUserPublic } from "@typing/user.interface";
-import { addLesson, isUser } from "@services/users.service";
+import { ObjectId } from 'bson';
+import { getDatabase } from './database.service';
+import { addLesson, isUser } from '@services/users.service';
+import { ILessonFile } from '@typing/lesson-file.interface';
+import { IUserPublic } from '@typing/user.interface';
 
 
 export const getAllLessons = async () => {
   try {
-    const collection = (await getDatabase()).collection<ILessonFile>("LessonFile");
+    const collection = (await getDatabase()).collection<ILessonFile>('LessonFile');
     return await collection.find({}).toArray();
   } catch (e) {
     return { error: e };
   }
 };
 
-export const createNewLesson = async (
-  user: IUserPublic,
-  {
-    title,
-    subtitle,
-    file,
-    // default values
-    creationDate = new Date(),
-    publicationDate,
-    isDraft = true,
-    // foreign keys
-    categoryIds = [],
-    tagIds = [],
-    commentIds = [],
-  }: ILessonFile
-): Promise<{ id: ObjectId } | { error: string }> => {
+export const createNewLesson = async (user: IUserPublic, {
+  title, subtitle, file, creationDate = new Date(), publicationDate, isDraft = true, // default values
+  categoryIds = [], tagIds = [], commentIds = [], // foreign keys
+}: ILessonFile): Promise<{ id: ObjectId } | { error: string }> => {
   try {
     if (!title || !isUser(user)) {
-      return { error: "Missing author or title." };
+      return { error: 'Missing author or title.' };
     }
 
-    const collection = (await getDatabase()).collection<ILessonFile>("LessonFile");
+    const collection = (await getDatabase()).collection<ILessonFile>('LessonFile');
 
     const lessonFile: ILessonFile = {
-      title,
-      subtitle,
-      file,
-      // meta data
-      creationDate,
-      publicationDate,
-      isDraft,
-      // foreign keys
-      authorId: user._id,
-      categoryIds,
-      tagIds,
-      commentIds,
+      title, subtitle, file, creationDate, publicationDate, isDraft, // meta data
+      authorId: user._id, categoryIds, tagIds, commentIds, // foreign keys
     };
 
     const result = await collection
@@ -59,21 +37,20 @@ export const createNewLesson = async (
 
     if (result.acknowledged) {
       // Adding it to the user's lessons
-      // user.lessonIds.push(result.insertedId);
       const updateResult = await addLesson(user, result.insertedId);
 
       if (updateResult.modifiedCount === 1) {
-        console.log(`[LESSON] Lesson upload successful! id: ${ result.insertedId }`, updateResult);
+        console.log(`[LESSON] Lesson upload successful! id: ${result.insertedId}`, updateResult);
         return { id: result.insertedId };
       } else {
-        const error = "Lesson upload failed! Write operation was not acknowledged.";
-        console.log(`[LESSON] ${ error }`);
+        const error = 'Lesson upload failed! Write operation was not acknowledged.';
+        console.log(`[LESSON] ${error}`);
         return { error };
       }
     }
   } catch (e) {
-    const error = `Lesson upload failed! There was an error: ${ e }`;
-    console.log(`[LESSON] ${ error }`);
+    const error = `Lesson upload failed! There was an error: ${e}`;
+    console.log(`[LESSON] ${error}`);
     return { error };
   }
 };
