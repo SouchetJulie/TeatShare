2fe3f33a31d9a1885819ba19f8fa7e34a1e1addf
EidanU <71008360+EidanU@users.noreply.github.com>
2022-01-12 21:20:31 +0100

    Changes by allan

--- src/client/components/LandingPage/Temoignage.tsx
+++ src/client/components/LandingPage/Temoignage.tsx
@@ -1,7 +1,9 @@
+import styles from '../../styles/LandingPage/Temoignage.module.scss';
+
 const Temoignage = () => {
     return (
-        <>
-        </>
+        <div className={styles.temoignage_Container}>
+        </div>
     );
 };
 export default Temoignage;
