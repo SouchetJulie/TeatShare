9e3526c490dad50e001cdc676a3bb6b1f49c7003
Julie Souchet <jsouchet@easyvista.com>
2022-01-12 19:23:46 +0100

    [session] Update iron-session

--- src/pages/api/user/logout.ts
+++ src/pages/api/user/logout.ts
@@ -1,16 +1,15 @@
-import { NextApiResponse } from 'next';
-import { defaultHandler } from '@common/default.handler';
-import { ISessionApiRequest } from '@typing/session-api-request.interface';
-import { logoutHandler } from '@handlers/user/logout/logout.handler';
+import {NextApiRequest, NextApiResponse} from 'next';
+import {defaultHandler} from '@common/default.handler';
+import {logoutHandler} from '@handlers/user/logout/logout.handler';
 
-export default async (req: ISessionApiRequest, res: NextApiResponse) => {
-  const handlers = {
-    'GET': logoutHandler,
-    'POST': logoutHandler,
-    // add here handlers for other methods
-  }
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const handlers = {
+        'GET': logoutHandler,
+        'POST': logoutHandler,
+        // add here handlers for other methods
+    }
 
-  const handler = handlers[req.method] || defaultHandler;
+    const handler = handlers[req.method] || defaultHandler;
 
-  await handler(req, res);
+    await handler(req, res);
 };
