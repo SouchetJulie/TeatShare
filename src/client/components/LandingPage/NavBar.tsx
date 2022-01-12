f4afa0c22f4b5219caf97454c4a1dd89cfa0b888
EidanU <71008360+EidanU@users.noreply.github.com>
2022-01-13 00:19:18 +0100

    [wip] landing page

--- src/client/components/LandingPage/NavBar.tsx
+++ src/client/components/LandingPage/NavBar.tsx
@@ -5,28 +5,37 @@ import styles from '../../styles/LandingPage/NavBar.module.scss'
 const NavBar = (): JSX.Element => {
     return (
         <div className={styles.container}>
-            <div>
+            <div className={styles.img_container}>
                 <Image
                     src={logo}
                     alt='Logo'
+                    layout='responsive'
                 />
             </div>
 
             <div className={styles.navigationLink}>
-                        <li>
-                            Se connecter
-                        </li>
-                        <li>
-                            S&apos;inscrire
-                        </li>
-                        <li>
-                            <Image
-                                src={logo}
-                                alt='avatar'
-                                width="40"
-                                height="40"
-                            />
-                        </li>
+                <li>
+                    Se connecter
+                </li>
+                <li>
+                    S&apos;inscrire
+                </li>
+                <li>
+                    <div className={styles.profileLiContainer}>
+                        <div className={styles.profilePicContainer}>
+                             <Image
+                                 src={logo}
+                                 alt='avatar'
+                                 layout='responsive'
+                                 object-position='center'
+                                 objectFit='cover'
+                                 width={10}
+                                 height={10}
+
+                             />
+                        </div>
+                    </div>
+                </li>
             </div>
         </div>
     );
