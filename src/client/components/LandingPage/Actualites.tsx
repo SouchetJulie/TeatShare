15588bca13583dccd2b47bb3b9d2ab382d39cc58
EidanU <71008360+EidanU@users.noreply.github.com>
2022-01-14 00:46:08 +0100

    [wip] landing page

--- src/client/components/LandingPage/Actualites.tsx
+++ src/client/components/LandingPage/Actualites.tsx
@@ -1,7 +1,31 @@
+import styles from '../../styles/LandingPage/Actualites.module.scss';
+import Link from "next/link";
+import cardData from '../../../public/FakeDatas/LandingPage.cards.json';
+import LandingCard from "./LandingCard";
+
 const Actualites = () => {
+
+    const cards = cardData.map((res,i) => (
+            <LandingCard
+                key={i}
+                course={res.course}
+                date={res.date}
+                img={res.img.replace('XXX', `cours${i + 1}.png`)}
+                matiere={res.matiere}
+                time={res.time}
+                title={res.title}
+                />));
+
     return (
-        <>
-        </>
+        <div className={styles.actualites_container}>
+            <h2>Nos Actualités</h2>
+            {cards}
+            <button>
+                <Link href={`/user/login`} >
+                    <a className={styles.connexion}>Voir tous les cours</a>
+                </Link>
+            </button>
+        </div>
     );
 };
 export default Actualites;
--- src/client/components/LandingPage/Footer.tsx
+++ src/client/components/LandingPage/Footer.tsx
@@ -1,7 +1,9 @@
+import styles from '../../styles/LandingPage/Footer.module.scss';
+
 const Footer = () => {
     return (
-        <>
-        </>
+        <div className={styles.landing_footer_container}>
+        </div>
     );
 };
 export default Footer;
