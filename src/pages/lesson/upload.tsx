f667cd294e0842b1da7c8deda0b54187f9c8a010
Julie Souchet <jsouchet@easyvista.com>
2022-01-16 17:51:52 +0100

    [store] Add example

--- src/pages/lesson/upload.tsx
+++ src/pages/lesson/upload.tsx
@@ -1,27 +1,101 @@
-import {FunctionComponent} from 'react';
+import axios from "axios";
+import {FormEvent, FunctionComponent, useState} from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import {useAppDispatch} from "@hooks/store-hook";
+import {addAlert} from "@stores/alert.store";
+import {ResourceApiResponse} from "@typing/api-response.interface";
+
+import styles from "./upload.module.scss";
 
 const upload: FunctionComponent = () => {
+  // store
+  const dispatch = useAppDispatch();
 
-    return (
-        <form action={'/api/lesson'} method="POST" encType="multipart/form-data">
-            <label htmlFor="lesson_upload_form_title">Titre</label>
-            <input id="lesson_upload_form_title" name="title" placeholder="Les tables de multiplication"/>
+  const [isDraft, setIsDraft] = useState(false);
 
-            <label htmlFor="lesson_upload_form_subtitle">Sous-titre</label>
-            <input id="lesson_upload_form_subtitle" name="subtitle" placeholder="Multiplier des nombres de 1 à 10..."/>
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    formData.append('isDraft', isDraft + "");
 
-            <label htmlFor="lesson_upload_form_file">Fichier</label>
-            <input
-                id="lesson_upload_form_file"
+    const {data} = await axios.post<ResourceApiResponse>('/api/lesson', formData);
+
+    const {success, id} = data;
+
+    if (success) {
+      dispatch(addAlert({message: `Lesson ${id} créée avec succès !`, success}));
+    }
+  }
+
+  return (
+    <>
+      <Container className="min-vh-100">
+        <Button onClick={() => dispatch(addAlert({message: "test", success: Math.random() > 0.5}))}>Test</Button>
+        <Row>
+          <h1 className="mt-4">Importer un PDF</h1>
+        </Row>
+        <Form className="my-5 row" onSubmit={onSubmit}>
+          {/* <form action={'/api/lesson'} method="POST" encType="multipart/form-data">*/}
+          <Col>
+            <Form.Group controlId="lesson_upload_form_title">
+              <Form.Label className={styles.label}>Titre</Form.Label>
+              <Form.Control
+                className={styles.control}
+                name="title"
+                placeholder="Ajoutez un titre"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="lesson_upload_form_subtitle">
+              <Form.Label className={styles.label}>Sous-titre</Form.Label>
+              <Form.Control
+                className={styles.control}
+                name="subtitle"
+                placeholder="Ajouter une courte description"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="lesson_upload_form_file">
+              <Form.Label className={styles.label}>Contenu de fiche</Form.Label>
+              <Form.Control
+                className={styles.control}
                 name="file"
                 type="file"
                 accept="application/pdf"
                 required
-            />
+              />
+            </Form.Group>
+          </Col>
 
-            <button>Envoyer</button>
-        </form>
-    );
+          <Col className="d-flex flex-column justify-content-center" sm="3">
+            <Button
+              className="round-button"
+              variant="primary"
+              type="submit"
+              onClick={() => setIsDraft(false)}
+            >
+              Soumettre
+            </Button>
+            <Button
+              className="round-button"
+              variant="secondary"
+              type="submit"
+              onClick={() => setIsDraft(true)}
+            >
+              Sauvegarder
+            </Button>
+          </Col>
+
+        </Form>
+      </Container>
+    </>
+  );
 };
 
 export default upload;
