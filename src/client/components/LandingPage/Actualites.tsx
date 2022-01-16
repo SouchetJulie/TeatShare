67c09fd6060742a53c1af111b4403418ac742de6
Allan <allan.duhe@edu.devinci.fr>
2022-01-16 22:33:46 +0100

    actu done

--- src/client/components/LandingPage/Actualites.tsx
+++ src/client/components/LandingPage/Actualites.tsx
@@ -5,35 +5,41 @@ import LandingCard from "./LandingCard";
 import Masonry from 'react-masonry-css';
 
 const Actualites = () => {
+  
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 2,
+    700: 2,
+    500: 1
+  };
 
-    const cards = cardData.map((res,i) => (
-            <LandingCard
-                key={i}
-                course={res.course}
-                date={res.date}
-                img={`${i}`}
-                matiere={res.matiere}
-                time={res.time}
-                title={res.title}
-                />));
+  const cards = cardData.map((res,i) => (
+    <LandingCard
+      key={i}
+      course={res.course}
+      date={res.date}
+      img={`${i}`}
+      matiere={res.matiere}
+      time={res.time}
+      title={res.title}
+    />));
 
-    return (
-        <div className={styles.actualites_container}>
-            <h2>Nos p’tites actus.</h2>
-            <Masonry
-                breakpointCols={3}
-                className={styles.myMasonryGrid}
-                columnClassName={styles.myMasonryGridColumn}
-            >
-                {cards}
-            </Masonry>
-            <button>
-                <Link href={`/user/login`} >
-                    <a className={styles.connexion}>Voir tous les cours</a>
-                </Link>
-            </button>
-
-        </div>
-    );
+  return (
+    <div className={styles.actualites_container}>
+      <h2>Nos Actualités</h2>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={styles.myMasonryGrid}
+        columnClassName={styles.myMasonryGridColumn}
+      >
+        {cards}
+      </Masonry>
+      <button className={styles.btnConnexion}>
+        <Link href={`/user/login`} >
+          <a>Voir tous les cours</a>
+        </Link>
+      </button>
+    </div>
+  );
 };
 export default Actualites;
--- src/client/components/LandingPage/LandingCard.tsx
+++ src/client/components/LandingPage/LandingCard.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import styles from '../../styles/LandingPage/Accueil.module.scss';
+import styles from '../../styles/LandingPage/Card.module.scss';
 import Link from "next/link";
 import Image from 'next/image';
 import {ILandingPageCardInterface} from "../../../types/ILandingPageCard.interface";
@@ -11,6 +11,8 @@ import img4 from '@imgCours/cours4.png';
 import img5 from '@imgCours/cours5.png';
 import img6 from '@imgCours/cours6.png';
 
+import timer from '../../../../public/landingPage/timer.svg';
+import styled from "styled-components";
 
 const LandingCard: FunctionComponent<ILandingPageCardInterface> = (
     {
@@ -21,6 +23,9 @@ const LandingCard: FunctionComponent<ILandingPageCardInterface> = (
         time,
         title
     }) =>{
+  const StyledButton = styled.a`
+    height: 200px;
+  `;
   const imgArray = [img1, img2, img3, img4, img5, img6];
     return(
         <div className={styles.cardContainer}>
@@ -28,6 +33,7 @@ const LandingCard: FunctionComponent<ILandingPageCardInterface> = (
                   <Image
                     src={imgArray[img]}
                     alt='Cours'
+                    layout='responsive'
                   />
             </div>
             <div className={styles.thematic}>
@@ -35,13 +41,15 @@ const LandingCard: FunctionComponent<ILandingPageCardInterface> = (
                 <span className={styles.course}>{course}</span>
             </div>
             <div className={styles.cardDate}>
-
-                <p>{time}, {date}</p>
+              <Image src={timer} alt='SVG Timer' />
+              <p>{time} , {date}</p>
             </div>
-            <div className={styles.cardTitle}>{title}</div>
+            <div className={styles.cardTitle}>
+              <p>{title}</p>
+            </div>
             <button>
                 <Link href={"/"} >
-                    <a className={styles.readMore}>Lire la suite</a>
+                    <StyledButton>Lire la suite</StyledButton>
                 </Link>
             </button>
         </div>
