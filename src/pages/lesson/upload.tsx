ff069ea289264c0eec8414e9c997160c8b7a6d88
Julie Souchet <jsouchet@easyvista.com>
2022-01-16 18:32:54 +0100

    [lesson] Prettify + add ajax for the upload form

--- src/pages/lesson/upload.tsx
+++ src/pages/lesson/upload.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import {FormEvent, FunctionComponent, useState} from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -11,6 +11,7 @@ import {addAlert} from "@stores/alert.store";
 import {ResourceApiResponse} from "@typing/api-response.interface";
 
 import styles from "./upload.module.scss";
+import {Alert} from "react-bootstrap";
 
 const upload: FunctionComponent = () => {
   // store
@@ -19,82 +20,95 @@ const upload: FunctionComponent = () => {
   const [isDraft, setIsDraft] = useState(false);
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
     formData.append('isDraft', isDraft + "");
 
-    const {data} = await axios.post<ResourceApiResponse>('/api/lesson', formData);
+    const {data} = await axios.post<ResourceApiResponse>('/api/lesson', formData)
+      .catch((e: AxiosError) => {
+        const data: ResourceApiResponse = {
+          success: false,
+          error: e.response.data.error || e.request || e.message
+        };
+        return {data};
+      });
 
-    const {success, id} = data;
+    const {success} = data;
 
     if (success) {
-      dispatch(addAlert({message: `Lesson ${id} créée avec succès !`, success}));
+      const {id} = data;
+      const message = <span>Leçon {isDraft ? 'sauvegardée' : 'créée'} avec succès ! <Alert.Link
+        href={`/api/lesson/${id}`}>(Voir la leçon)</Alert.Link> </span>
+
+      dispatch(addAlert({message, success}));
+    } else {
+      const {error} = data;
+      dispatch(addAlert({message: `Création de la branche échouée : ${error}`, success}));
     }
   }
 
   return (
-    <>
-      <Container className="min-vh-100">
-        <Button onClick={() => dispatch(addAlert({message: "test", success: Math.random() > 0.5}))}>Test</Button>
-        <Row>
-          <h1 className="mt-4">Importer un PDF</h1>
-        </Row>
-        <Form className="my-5 row" onSubmit={onSubmit}>
-          {/* <form action={'/api/lesson'} method="POST" encType="multipart/form-data">*/}
-          <Col>
-            <Form.Group controlId="lesson_upload_form_title">
-              <Form.Label className={styles.label}>Titre</Form.Label>
-              <Form.Control
-                className={styles.control}
-                name="title"
-                placeholder="Ajoutez un titre"
-                required
-              />
-            </Form.Group>
+    <Container className="min-vh-100">
+      <Row>
+        <h1 className="mt-4">Importer un PDF</h1>
+      </Row>
+      <Form className="my-5 row" onSubmit={onSubmit} method="POST">
+        <Col>
+          <Form.Group controlId="lesson_upload_form_title">
+            <Form.Label className={styles.label}>Titre</Form.Label>
+            <Form.Control
+              className={styles.control}
+              name="title"
+              placeholder="Ajoutez un titre"
+              required
+            />
+          </Form.Group>
 
-            <Form.Group controlId="lesson_upload_form_subtitle">
-              <Form.Label className={styles.label}>Sous-titre</Form.Label>
-              <Form.Control
-                className={styles.control}
-                name="subtitle"
-                placeholder="Ajouter une courte description"
-              />
-            </Form.Group>
+          <Form.Group controlId="lesson_upload_form_subtitle">
+            <Form.Label className={styles.label}>Sous-titre</Form.Label>
+            <Form.Control
+              className={styles.control}
+              name="subtitle"
+              placeholder="Ajouter une courte description"
+            />
+          </Form.Group>
 
-            <Form.Group controlId="lesson_upload_form_file">
-              <Form.Label className={styles.label}>Contenu de fiche</Form.Label>
-              <Form.Control
-                className={styles.control}
-                name="file"
-                type="file"
-                accept="application/pdf"
-                required
-              />
-            </Form.Group>
-          </Col>
+          <Form.Group controlId="lesson_upload_form_file">
+            <Form.Label className={styles.label}>Contenu de fiche</Form.Label>
+            <Form.Control
+              className={styles.control}
+              name="file"
+              type="file"
+              accept="application/pdf"
+              required
+            />
+          </Form.Group>
+        </Col>
 
-          <Col className="d-flex flex-column justify-content-center" sm="3">
-            <Button
-              className="round-button"
-              variant="primary"
-              type="submit"
-              onClick={() => setIsDraft(false)}
-            >
-              Soumettre
-            </Button>
-            <Button
-              className="round-button"
-              variant="secondary"
-              type="submit"
-              onClick={() => setIsDraft(true)}
-            >
-              Sauvegarder
-            </Button>
-          </Col>
+        <Col className="d-flex flex-column justify-content-center" sm="3">
+          <Button
+            className="round-button"
+            variant="primary"
+            type="submit"
+            onClick={() => setIsDraft(false)}
+          >
+            Soumettre
+          </Button>
+          <Button
+            className="round-button"
+            variant="secondary"
+            type="submit"
+            onClick={() => setIsDraft(true)}
+          >
+            Sauvegarder
+          </Button>
+        </Col>
 
-        </Form>
-      </Container>
-    </>
+      </Form>
+    </Container>
   );
 };
 
--- src/server/handlers/lesson/post.handler.ts
+++ src/server/handlers/lesson/post.handler.ts
@@ -8,55 +8,66 @@ import {IUserPublic} from '@typing/user.interface';
 import {ILessonCreate} from '@typing/lesson-file.interface';
 
 interface LessonFormData {
-    fields?: Fields;
-    files?: Files;
+  fields?: Fields;
+  files?: Files;
 }
 
 const handler = async (req: ISessionApiRequest, res: NextApiResponse) => {
-    try {
-        const formData: LessonFormData & { error?: string } = await new Promise((resolve, reject) => {
-            const form = new IncomingForm({
-                hashAlgorithm: 'sha256',
-                multiples: false,
-                filter: ({mimetype}: Part) => mimetype && mimetype.includes('pdf'), // keep only pdf
-            });
+  try {
+    const formData: LessonFormData & { error?: string } = await new Promise((resolve, reject) => {
+      const form = new IncomingForm({
+        hashAlgorithm: 'sha256',
+        multiples: false,
+        filter: ({mimetype}: Part) => mimetype && mimetype.includes('pdf'), // keep only pdf
+      });
 
-            form.parse(req, (err, fields: Fields, files: Files) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve({fields, files});
-            });
-        })
-            .catch((err) => ({error: err}));
-
-        // Error handling
-        if (formData.error) {
-            return res.status(400).json({error: formData.error});
+      form.parse(req, (err, fields: Fields, files: Files) => {
+        if (err) {
+          return reject(err);
         }
+        resolve({fields, files});
+      });
+    })
+      .catch((err) => ({error: err}));
 
-        // Get author
-        const currentUser = req.session.get<IUserPublic>('user');
-
-        // Get file
-        let file: File;
-        if (formData?.files?.file instanceof Array) {
-            file = formData.files.file[0];
-        } else {
-            file = formData.files.file;
-        }
-
-        const uploadedLesson: ILessonCreate = formData.fields;
-
-        const result = await createNewLesson(currentUser, file, uploadedLesson);
-        if (result['id']) {
-            return res.status(200).json({success: true, id: result['id']});
-        } else {
-            return res.status(400).json({success: false, error: result['error']});
-        }
-    } catch (e) {
-        return res.status(400).json({success: false, error: e.message});
+    // Error handling
+    if (formData.error) {
+      return res.status(400).json({error: formData.error});
     }
+
+    // Get author
+    const currentUser = req.session.get<IUserPublic>('user');
+
+    // Get file
+    let file: File;
+    if (formData?.files?.file instanceof Array) {
+      file = formData.files.file[0];
+    } else {
+      file = formData.files.file;
+    }
+
+    // Get isDraft
+    let isDraft: string;
+    if (formData?.fields.isDraft instanceof Array) {
+      isDraft = formData?.fields.isDraft[0];
+    } else {
+      isDraft = formData?.fields.isDraft;
+    }
+
+    const uploadedLesson: ILessonCreate = {
+      ...formData.fields,
+      isDraft: JSON.parse(isDraft) // conversion to boolean
+    };
+
+    const result = await createNewLesson(currentUser, file, uploadedLesson);
+    if (result['id']) {
+      return res.status(200).json({success: true, id: result['id']});
+    } else {
+      return res.status(400).json({success: false, error: result['error']});
+    }
+  } catch (e) {
+    return res.status(400).json({success: false, error: e.message});
+  }
 };
 
 export const lessonPostHandler = withSession(handler);
