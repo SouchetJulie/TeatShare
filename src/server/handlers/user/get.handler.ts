6a2e031a94be7f98e64d472152dabc2b562d547d
Julie Souchet <jsouchet@easyvista.com>
2022-01-18 15:44:31 +0100

    [internal] Move createEmptyUser to separate file

--- src/server/handlers/user/get.handler.ts
+++ src/server/handlers/user/get.handler.ts
@@ -1,17 +1,21 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-import {getAllUsers, getOneUser} from '@services/users.service';
+import { NextApiRequest, NextApiResponse } from "next";
+import { getAllUsers, getOneUser } from "@services/users.service";
 
-export const userGetAllHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const users = await getAllUsers();
-    res.status(200).json({users});
+export const userGetAllHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const users = await getAllUsers();
+  res.status(200).json({ users });
 };
 
-export const userGetOneHandler = (_id: string) => async (req: NextApiRequest, res: NextApiResponse) => {
+export const userGetOneHandler =
+  (_id: string) => async (req: NextApiRequest, res: NextApiResponse) => {
     const result = await getOneUser(_id);
 
-    if ('error' in result) {
-        return res.status(404).json({success: false, error: result.error});
+    if ("error" in result) {
+      return res.status(404).json({ success: false, error: result.error });
     }
 
-    return res.status(200).json({success: true, lesson: result});
-};
+    return res.status(200).json({ success: true, user: result });
+  };
