f8dc647bcad44441e433bb4ab3cacde6b01db37f
Julie Souchet <jsouchet@easyvista.com>
2022-01-18 19:00:26 +0100

    login

--- src/client/components/signIn_signUp/LoginForm.component.tsx
+++ src/client/components/signIn_signUp/LoginForm.component.tsx
@@ -1,27 +1,85 @@
-import { FunctionComponent } from "react";
+import { FormEventHandler, FunctionComponent, useState } from "react";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../../styles/Login.Component.module.scss";
+import Link from "next/link";
+import { Button, Form } from "react-bootstrap";
+import axios from "axios";
+import { LoginRequest } from "@typing/login-request.interface";
+import { useRouter } from "next/router";
 
 interface LoginFormComponentProps {}
 
 const LoginFormComponent: FunctionComponent<LoginFormComponentProps> = () => {
+  const [validated, setValidated] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (
+    event
+  ): Promise<void> => {
+    setValidated(true);
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    if (form.checkValidity()) {
+      const formData: FormData = new FormData(form);
+      const user: LoginRequest = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+      };
+      if (user.email && user.password) {
+        axios({
+          method: "POST",
+          url: "/api/user/login",
+          data: user,
+        })
+          .then(function (response) {
+            router.push("/");
+          })
+          .catch(function (error) {
+            alert("failed");
+          });
+      }
+    } else {
+      // TODO Add alert store
+    }
+  };
+  const handleFocus = (): void => {};
   return (
-    <div className="cardWrapper">
-      <div className="cardContainer">
-        <div className="cancelIcon">
-          <FontAwesomeIcon icon={faTimes} />
+    <div className={styles.cardWrapper}>
+      <div className={styles.cardContainer}>
+        <div className={styles.cancelIcon}>
+          <Link href="/">
+            <FontAwesomeIcon icon={faTimes} />
+          </Link>
         </div>
         <h2>Se connecter</h2>
-        <form action="/api/user/login" method="POST">
-          <input
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+          action={"/api/user/login"}
+          method="POST"
+          className={styles.loginForm}
+        >
+          {/* Email */}
+          <Form.Label htmlFor="email" className={styles.invisibleLabel}>
+            Email :
+          </Form.Label>
+          <Form.Control
+            className={styles.loginInput}
             id="email"
             name="email"
             placeholder="adresse email"
             type="email"
             required
           />
-
-          <input
+          {/* Password */}
+          <Form.Label htmlFor="password" className={styles.invisibleLabel}>
+            Mot de passe :
+          </Form.Label>
+          <Form.Control
+            className={styles.loginInput}
             id="password"
             name="password"
             placeholder="mot de passe"
@@ -29,33 +87,28 @@ const LoginFormComponent: FunctionComponent<LoginFormComponentProps> = () => {
             required
           />
 
-          <button type="submit">Envoyer</button>
-          <button type="submit">Créer un compte</button>
-        </form>
+          {/* Validation */}
+          <Button
+            className={styles.loginButtonSend}
+            type="submit"
+            onFocus={handleFocus}
+          >
+            Valider
+          </Button>
+
+          {/* Création de compte -> Redirection */}
+          <button
+            className={styles.loginRedirectSignup}
+            type="button"
+            onFocus={handleFocus}
+          >
+            <Link href={"/user/signup"}>
+              <a>Créer un compte</a>
+            </Link>
+          </button>
+        </Form>
       </div>
     </div>
   );
 };
 export default LoginFormComponent;
-/**    <form action={'/api/user/login'} method="POST">
- <label htmlFor="email">Email :</label>
- <input
- id="email"
- name="email"
- placeholder="Entrez votre email"
- type="email"
- required
- />
- 
- <label htmlFor="password">Mot de passe :</label>
- <input
- id="password"
- name="password"
- placeholder="Entrez votre mot de passe"
- type="password"
- required
- />
- 
- <button type="submit">Envoyer</button>
- </form>
- **/
--- src/server/handlers/user/login/login.handler.ts
+++ src/server/handlers/user/login/login.handler.ts
@@ -1,39 +1,45 @@
-import {NextApiHandler, NextApiRequest, NextApiResponse} from 'next';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-import {checkCredentials, getUserByEmail, isUser} from '@services/users.service';
-import {withSession} from '@middlewares/session.middleware';
+import {
+  checkCredentials,
+  getUserByEmail,
+  isUser,
+} from "@services/users.service";
+import { withSession } from "@middlewares/session.middleware";
+import { LoginRequest } from "@typing/login-request.interface";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const userCredentials = req.body;
-    if (!userCredentials.password || !userCredentials.email) {
-        res.status(400).json({error: 'Veuillez remplir le formulaire.'});
+  const userCredentials = req.body as LoginRequest;
+  console.log(req.body);
+  if (!userCredentials.password || !userCredentials.email) {
+    return res.status(400).json({ error: "Veuillez remplir les champs !" });
+  }
+
+  const result = await checkCredentials(userCredentials);
+  if (result) {
+    console.log(`[LOGIN] Credentials OK for ${userCredentials.email}.`);
+    const user = await getUserByEmail(userCredentials.email);
+
+    if (!isUser(user)) {
+      return res.status(400).json({
+        success: false,
+        error: user["error"] || "Login failed",
+      });
     }
 
-    const result = await checkCredentials(userCredentials);
-    if (result) {
-        console.log(`[LOGIN] Credentials OK for ${userCredentials.email}.`);
-        const user = await getUserByEmail(userCredentials.email);
+    req.session.user = user;
+    await req.session.save();
+    console.log(`[LOGIN] Started session for ${userCredentials.email}.`);
 
-        if (!isUser(user)) {
-            return res.status(400).json({
-                success: false,
-                error: user['error'] || 'Login failed'
-            });
-        }
-
-        req.session.user = user;
-        await req.session.save();
-        console.log(`[LOGIN] Started session for ${userCredentials.email}.`);
-
-        res.status(200).json({
-            success: true
-        });
-    } else {
-        res.status(400).json({
-            success: false,
-            error: result['error'] || 'Login failed'
-        });
-    }
+    return res.status(200).json({
+      success: true,
+    });
+  } else {
+    return res.status(400).json({
+      success: false,
+      error: result["error"] || "Login failed",
+    });
+  }
 };
 
 export const loginHandler: NextApiHandler = withSession(handler);
