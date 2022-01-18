ccc3e5f8ce0da7b2c308d26ffb180c967612955a
EidanU <71008360+EidanU@users.noreply.github.com>
2022-01-18 12:33:29 +0100

    [WIP] LandingPage responsive

--- src/client/components/LandingPage/Accueil.tsx
+++ src/client/components/LandingPage/Accueil.tsx
@@ -1,56 +1,65 @@
-import Link from 'next/link';
-import styles from '../../styles/LandingPage/Accueil.module.scss';
+import Link from "next/link";
+import styles from "../../styles/LandingPage/Accueil.module.scss";
 
 const Accueil = () => {
-    return (
-        <div className={styles.accueil_container}>
-            <div className={styles.bloc1}>
-                <div className={styles.block1_Content}>
-                    <h1> Vous êtes instituteur ?</h1>
-                    <p> Vous voudriez partager vos cours ou vous êtes <br/>à la recherche de l&apos;inspiration ?</p>
-                    <button>
-                        <Link href={`/user/login`} >
-                            <a className={styles.connexion}>Se connecter</a>
-                        </Link>
-                    </button>
-                </div>
-            </div>
-            <div className={styles.bloc2}>
-                <div className={styles.block2Content}>
-                    <div className={styles.leftSide}>
-                        <div className={styles.imessage}>
-                            <p className={styles.fromMe}>Je n&apos;ai pas<br/> d&apos;inspiration...</p>
-                            <div className={styles.queue}></div>
-                        </div>
-                        <h2>Non, Facile!</h2>
-                    </div>
-
-                    <div className={styles.rightSide}>
-                        <h2>Trouvez-en.</h2>
-                        <div className={styles.imessage}>
-                            <p className={styles.fromMe}>Créer un cours<br/> c&apos;est compliqué !!</p>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={styles.bottomSide}>
-                    <p>Le cours vous a t&apos;il aidé ?</p>
-                    <div className={styles.choice}>
-                        <div className={styles.yesPart}>
-                            <p>OUI</p>
-                        </div>
-                        <div className={styles.noPart}>
-                            <p>NON</p>
-                        </div>
-                    </div>
-                    <button className={styles.callToAction}>
-                        <Link href={`/user/signup`} >
-                            <a className={styles.participate}>Participer</a>
-                        </Link>
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className={styles.accueil_container}>
+      <div className={styles.bloc1}>
+        <div className={styles.block1Content}>
+          <h1> Vous êtes instituteur ?</h1>
+          <p>
+            {" "}
+            Vous voudriez partager vos cours ou vous êtes <br />à la recherche
+            de l&apos;inspiration ?
+          </p>
+          <button>
+            <Link href={`/user/login`}>
+              <a className={styles.connexion}>Se connecter</a>
+            </Link>
+          </button>
         </div>
-    );
+      </div>
+      <div className={styles.bloc2}>
+        <div className={styles.block2Content}>
+          <div className={styles.leftSide}>
+            <div className={styles.imessage}>
+              <p className={styles.fromMe}>
+                Je n&apos;ai pas
+                <br /> d&apos;inspiration...
+              </p>
+              <div className={styles.queue}></div>
+            </div>
+            <h2>Non, Facile!</h2>
+          </div>
+
+          <div className={styles.rightSide}>
+            <h2>Trouvez-en.</h2>
+            <div className={styles.imessage}>
+              <p className={styles.fromMe}>
+                Créer un cours
+                <br /> c&apos;est compliqué !!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bottomSide}>
+          <p>Le cours vous a t&apos;il aidé ?</p>
+          <div className={styles.choice}>
+            <div className={styles.yesPart}>
+              <p>OUI</p>
+            </div>
+            <div className={styles.noPart}>
+              <p>NON</p>
+            </div>
+          </div>
+          <button className={styles.callToAction}>
+            <Link href={`/user/signup`}>
+              <a className={styles.participate}>Participer</a>
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Accueil;
