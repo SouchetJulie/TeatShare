faece6b9db1e8f582c026fe7261ccb3f16a954bc
Julie Souchet <jsouchet@easyvista.com>
2022-01-19 14:48:21 +0100

    [storage] Change credentials format

--- src/server/services/storage.service.ts
+++ src/server/services/storage.service.ts
@@ -1,13 +1,15 @@
 import { Storage } from "@google-cloud/storage";
 
-console.log(process.env.private_key); // TODO to remove
+const credentials = JSON.parse(process.env.gcs_credentials);
+
+console.log(credentials.private_key); // TODO to remove
 const storage = new Storage({
   credentials: {
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    private_key: process.env.private_key,
+    client_email: credentials.client_email,
+    client_id: credentials.client_id,
+    private_key: credentials.private_key,
   },
-  projectId: process.env.project_id,
+  projectId: credentials.project_id,
 });
 const bucket = process.env.BUCKET_NAME;
 
