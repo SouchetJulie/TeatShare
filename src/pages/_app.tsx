1c9b523df3977b730c2171e2d537b11e6a38e7e7
Julie Souchet <jsouchet@easyvista.com>
2022-01-21 12:35:32 +0100

    [style] Fix sidebar overlapping with main content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,9 @@ import Alert from "@components/AlertComponent";
 import NavBar from "@components/Menu/NavBar";
 import SideBar from "@components/Menu/SideBar";
 import { store } from "@stores/store";
-import { IAlert } from "@stores/alert.store";
+import { IAlert, selectAlerts } from "@stores/alert.store";
 import "@styles/globals.scss";
+import { selectIsAuthenticated } from "@stores/user.store";
 
 /**
  * Main application component: contains the parts that are in common for the whole app.
@@ -16,9 +17,12 @@ import "@styles/globals.scss";
  */
 function App({ Component, pageProps }: AppProps) {
   const [alertList, setAlertList] = useState<IAlert[]>([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   store.subscribe(() => {
-    setAlertList(store.getState().alerts.list);
+    const state = store.getState();
+    setAlertList(selectAlerts(state));
+    setIsAuthenticated(selectIsAuthenticated(state));
   });
 
   const alerts = alertList.map((alert: IAlert) => {
@@ -45,7 +49,10 @@ function App({ Component, pageProps }: AppProps) {
           <NavBar />
           <SideBar />
         </header>
-        <main id="__next_page">
+        <main
+          id="__next_page"
+          className={isAuthenticated ? "authenticated" : ""}
+        >
           <Component {...pageProps} />
         </main>
         <div className="d-flex flex-column position-fixed bottom-0 w-100 onTop">
