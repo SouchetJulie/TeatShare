140a521542641c2974fcb531727af125205d01aa
Julie Souchet <jsouchet@easyvista.com>
2022-01-21 14:06:28 +0100

    [internal] Fix sidebar links component

--- src/client/components/Menu/SideBar.tsx
+++ src/client/components/Menu/SideBar.tsx
@@ -3,13 +3,14 @@ import Nav from "react-bootstrap/Nav";
 import styles from "@styles/Menu/navbar.module.scss";
 import { useSelector } from "react-redux";
 import { selectIsAuthenticated } from "@stores/user.store";
+import LessonLinks from "@components/Menu/LessonLinks";
 
 const SideBar: FunctionComponent = (): JSX.Element => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const style: string = isAuthenticated ? styles.sidebar : "";
   return (
     <Nav className={style} aria-hidden hidden={!isAuthenticated}>
-      <sidebarContent />
+      <LessonLinks />
     </Nav>
   );
 };
