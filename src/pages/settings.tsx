bffbeca07ec196f0b295fea5b5c74b0ee056133e
Julie Souchet <jsouchet@easyvista.com>
2022-01-21 11:24:20 +0100

    Small fixes

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,10 +1,10 @@
 import React, { FunctionComponent } from "react";
 import { useLoginRedirect } from "@hooks/useLoginRedirect.hook";
 
-const index: FunctionComponent = () => {
+const settings: FunctionComponent = () => {
   const user = useLoginRedirect();
 
   return user ? <div>TODO</div> : <></>;
 };
 
-export default index;
+export default settings;
