de248143ac135ae010874031053f9677346b9d84
Julie Souchet <jsouchet@easyvista.com>
2022-01-30 10:08:47 +0100

    [client] Load SVGs as React components

--- src/client/components/LandingPage/LandingCard.tsx
+++ src/client/components/LandingPage/LandingCard.tsx
@@ -1,8 +1,8 @@
-import { FunctionComponent } from "react";
+import {FunctionComponent} from "react";
 import styles from "../../styles/LandingPage/Card.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import { ILandingPageCardInterface } from "../../../types/ILandingPageCard.interface";
+import {ILandingPageCardInterface} from "@typing/ILandingPageCard.interface";
 
 import img1 from "@imgCours/cours1.png";
 import img2 from "@imgCours/cours2.png";
@@ -11,28 +11,28 @@ import img4 from "@imgCours/cours4.png";
 import img5 from "@imgCours/cours5.png";
 import img6 from "@imgCours/cours6.png";
 
-import timer from "../../../../public/landingPage/timer.svg";
+import Timer from "../../../../public/landingPage/timer.svg";
 
 const LandingCard: FunctionComponent<ILandingPageCardInterface> = ({
-  course,
-  date,
-  img,
-  matiere,
-  time,
-  title,
-}) => {
+                                                                     course,
+                                                                     date,
+                                                                     img,
+                                                                     matiere,
+                                                                     time,
+                                                                     title,
+                                                                   }) => {
   const imgArray = [img1, img2, img3, img4, img5, img6];
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardImg}>
-        <Image src={imgArray[img]} alt="Cours" layout="responsive" />
+        <Image src={imgArray[img]} alt="Cours" layout="responsive"/>
       </div>
       <div className={styles.thematic}>
         <span className={styles.matiere}>{matiere}</span>
         <span className={styles.course}>{course}</span>
       </div>
       <div className={styles.cardDate}>
-        <Image src={timer} alt="SVG Timer" />
+        <Timer/>
         <p>
           {time} , {date}
         </p>
