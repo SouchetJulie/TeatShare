ef97129ad9b6d0c5d449daf5afdc9d89b8a52ab6
Julie Souchet <jsouchet@easyvista.com>
2022-03-18 19:56:07 +0100

    Fix Vercel cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "react-redux";
 
-import Footer from "@components/menu_TORENAME/Footer";
-import NavBar from "@components/menu_TORENAME/NavBar";
-import SideBar from "@components/menu_TORENAME/SideBar";
+import Footer from "@components/menu/Footer";
+import NavBar from "@components/menu/NavBar";
+import SideBar from "@components/menu/SideBar";
 import Alert from "@components/ui/AlertComponent";
 import { store } from "@stores/store";
 import { IAlert, selectAlerts } from "@stores/alert.store";
