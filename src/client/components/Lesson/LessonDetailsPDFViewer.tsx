0ba29468a460ca91a32709417064709a329d7113
Allan <allan.duche@gmail.com>
2022-05-20 09:45:05 +0200

    [PDF Viewer] Init react-pdf

---
+++ src/client/components/Lesson/LessonDetailsPDFViewer.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+import { Document, Page } from 'react-pdf';
+
+interface OwnProps {}
+
+const LessonDetailsPDFViewer: FunctionComponent<OwnProps> = (
+  {
+  
+  }
+): JSX.Element => {
+  return ();
+};
+
+export default LessonDetailsPDFViewer;
