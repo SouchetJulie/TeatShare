01fab7b9715f70362740b2f249c8941692659089
Julie Souchet <jsouchet@easyvista.com>
2022-06-11 15:34:11 +0200

    [client] Display current user info (no style)

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -4,7 +4,23 @@ import { useLoginRedirect } from "@hooks/login-redirect.hook";
 const index: FunctionComponent = () => {
   const user = useLoginRedirect();
 
-  return user ? <div>TODO</div> : <></>;
+  return user ? (
+    <div>
+      TODO debug
+      <table>
+        {Object.entries(user).map(([key, value]) => {
+          return (
+            <tr>
+              <td>{key}</td>
+              <td>{Array.isArray(value) ? value.join(", ") : value}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default index;
