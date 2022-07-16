af7a85e1487584921dc48649e7f79db2f291cf06
Julie Souchet <jsouchet@easyvista.com>
2022-07-16 11:31:05 +0200

    [api] Fix "no response, stalled request" message

--- src/pages/api/user/bookmark/[_id].ts
+++ src/pages/api/user/bookmark/[_id].ts
@@ -4,6 +4,12 @@ import routerMiddleware from "@middlewares/router.middleware";
 import { ApiResponse } from "@typing/api-response.interface";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { _id } = req.query as { _id: string };
   const handlers: Record<string, NextApiHandler<ApiResponse>> = {
