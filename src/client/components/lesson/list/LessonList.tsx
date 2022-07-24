cce9b484203b95fb0d65296c750c58bc36bd9a64
Julie Souchet <jsouchet@easyvista.com>
2022-07-24 10:50:14 +0200

    [front] Add title to lesson pages

--- src/client/components/lesson/list/LessonList.tsx
+++ src/client/components/lesson/list/LessonList.tsx
@@ -2,16 +2,22 @@ import { useLessonFilterReducer } from "@components/lesson/list/lesson-filter-re
 import { LessonListFilter } from "@components/lesson/list/LessonListFilter.component";
 import styles from "@styles/lesson/lesson-list.module.scss";
 import { ILesson } from "@typing/lesson.interface";
+import Head from "next/head";
 import { FunctionComponent } from "react";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import LessonItem from "./LessonItem";
 
 interface Props {
   lessons: ILesson[];
+  title: string;
 }
 
-export const LessonList: FunctionComponent<Props> = ({ lessons }: Props) => {
+export const LessonList: FunctionComponent<Props> = ({
+  lessons,
+  title,
+}: Props) => {
   const [filters, filterDispatch] = useLessonFilterReducer();
 
   const filteredLessons = lessons.filter((lesson: ILesson) => {
@@ -33,13 +39,23 @@ export const LessonList: FunctionComponent<Props> = ({ lessons }: Props) => {
   });
 
   return (
-    <Container className="my-4 mb-5">
-      <LessonListFilter filters={filters} filterDispatch={filterDispatch} />
-      <Row className={`${styles.lessonContainer} my-4`}>
-        {filteredLessons.map((lesson: ILesson) => (
-          <LessonItem key={`lesson-item-${lesson._id}`} lesson={lesson} />
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Head>
+        <title>TeatShare - {title}</title>
+      </Head>
+      <Container>
+        <Row className="mt-5 justify-content-center">
+          <Col sm="auto" as="h1" className="text-secondary">
+            {title}
+          </Col>
+        </Row>
+        <LessonListFilter filters={filters} filterDispatch={filterDispatch} />
+        <Row className={`${styles.lessonContainer} my-4`}>
+          {filteredLessons.map((lesson: ILesson) => (
+            <LessonItem key={`lesson-item-${lesson._id}`} lesson={lesson} />
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 };
--- src/pages/lesson/index.tsx
+++ src/pages/lesson/index.tsx
@@ -2,17 +2,12 @@ import { LessonList } from "@components/lesson/list/LessonList";
 import { useAutoLogin } from "@hooks/auto-login.hook";
 import { useFetchLessons } from "@hooks/fetch-lessons.hook";
 import { FunctionComponent } from "react";
-import { Container } from "react-bootstrap";
 
 const index: FunctionComponent = () => {
   const user = useAutoLogin();
   const { lessons } = useFetchLessons({ author: user?._id });
 
-  return (
-    <Container>
-      <LessonList lessons={lessons} />
-    </Container>
-  );
+  return <LessonList lessons={lessons} title="Mes leçons" />;
 };
 
 export default index;
